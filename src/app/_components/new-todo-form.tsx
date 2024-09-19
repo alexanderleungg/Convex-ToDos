3d23e257a5ac740a7f45bccbd90219338d05f3a4
alexanderleungg <alexanderleungg>
2024-09-19 04:16:18 -0700

    second commit

---
+++ src/app/_components/new-todo-form.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+
+export function NewToDoForm() {
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const createTodo = useMutation(api.functions.createTodo);
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        await createTodo({ title, description });
+        setTitle("");
+        setDescription("");
+      };
+
+    return (
+
+        <form onSubmit = {handleSubmit}>
+            <div className = "flex flex-col gap-2">
+                <label className = "text-sm font-semibold" htmlFor = "title">Title</label>
+                <input 
+                    className = "p-1 border rounded" 
+                    type = "text" 
+                    name = "title" 
+                    id = "title" 
+                    value = {title} 
+                    onChange = {e => setTitle(e.target.value)}
+                />
+                <label className = "text-sm font-semibold" htmlFor = "description">Description</label>
+                <input
+                    className = "p-1 border rounded" 
+                    type = "text" 
+                    name = "description" 
+                    id = "description" 
+                    value = {description} 
+                    onChange = {e => setDescription(e.target.value)}
+                />
+                <button className = "bg-blue-500 p-1 rounded text-white" type = "submit">Create</button>
+            </div>
+      </form>
+    )
+}
